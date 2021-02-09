f552feb0c40f4cb21d174722e34ba1ed2206b65b
Magali Garot <magali.garot@gmail.com>
2021-02-09 11:27:31 +0100

    serveur

--- src/server/config.js
+++ src/server/config.js
@@ -1,3 +1,12 @@
-module.exports = {
-    DBCONNECTION : "mongodb+srv://<magali>:<corses400>@cluster0.syldx.mongodb.net/<LesCorses>?retryWrites=true&w=majority"
-}
+// mongoose.Promise = global.Promise;
+// mongoose.connect(
+//     "mongodb+srv://magali:corses400@cluster0.syldx.mongodb.net/LesCorses?retryWrites=true&w=majority",
+//     {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useUnifiedTopology: true,
+//     },
+// );
+// mongoose.connection.on("error", () => {
+//     throw new Error(`Unable to connect to database`);
+// });
--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,7 @@
 
 import express from "express";
 import path from "path";
+// import "config.js";
 
 const {APP_PORT} = process.env;
 
