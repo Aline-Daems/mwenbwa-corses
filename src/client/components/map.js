bfdd2c19a7c5e3c0c188fae428f3d8252e2206bd
Magali Garot <magali.garot@gmail.com>
2021-02-23 17:10:31 +0100

    profilpopup

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -1,49 +1,61 @@
 /* ICON TREE artist => https://www.flaticon.com/fr/icone-gratuite/forme-darbre_41409 */
+// import React from "react";
+// const ReactDOM = require("react-dom");
+// import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+// import MarkerClusterGroup from "react-leaflet-markercluster";
+// import treesData from "../../../data/arbustum.json";
+// import iconBlack from "../../images/flaticonblack.png";
+// import L from "leaflet";
+// import ProfilModal from './profil.js';
 
-import React from "react";
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-markercluster";
-import treesData from "../../../data/arbustum.json";
-import iconBlack from "../../images/flaticonblack.png";
-import L from "leaflet";
+// //ICON TREE //
+// const markerIcon = new L.Icon({
+//     iconUrl: iconBlack,
+//     iconSize: [35, 45],
+// });
 
-//ICON TREE //
-const markerIcon = new L.Icon({
-    iconUrl: iconBlack,
-    iconSize: [35, 45],
-});
+// const TreesMap = () => {
+//     const filterTree = treesData.filter(
+//         tree => tree.y_phi != null && tree.x_lambda != null,
+//     );
+//     return (
+//         <MapContainer
+//             center={[50.630236875737616, 5.548094622321533]}
+//             zoom={13}
+//             scrollWheelZoom={true}>
+//             <TileLayer
+//                 attribution={
+//                     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//                 }
+//                 url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+//             />
+//             <MarkerClusterGroup disableClusteringAtZoom={19}>
+//                 {filterTree.map((tree, arbotag) => (
+//                     <Marker
+//                         // eslint-disable-next-line react/no-array-index-key
+//                         key={arbotag}
+//                         position={[tree.y_phi, tree.x_lambda]}
+//                         icon={markerIcon}>
+//                         <Popup>
+//                             {[tree.nom_complet]} <br />
+//                             {"Nom du jouer"}<br />
+//                             {"Nombre de feuilles"}
+//                         </Popup>
+//                     </Marker>
+//                 ))}
+//             </MarkerClusterGroup>
+//         </MapContainer>
+//     );
+// };
 
-const TreesMap = () => {
-    const filterTree = treesData.filter(
-        tree => tree.y_phi != null && tree.x_lambda != null,
-    );
-    return (
-        <MapContainer
-            center={[50.630236875737616, 5.548094622321533]}
-            zoom={13}
-            scrollWheelZoom={true}>
-            <TileLayer
-                attribution={
-                    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                }
-                url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
-            />
-            <MarkerClusterGroup disableClusteringAtZoom={19}>
-                {filterTree.map((tree, arbotag) => (
-                    <Marker
-                        // eslint-disable-next-line react/no-array-index-key
-                        key={arbotag}
-                        position={[tree.y_phi, tree.x_lambda]}
-                        icon={markerIcon}>
-                        <Popup>
-                            {[tree.nom_complet]} <br />
-                            {"Nom du jouer"}
-                            {"Nombre de feuilles"}
-                        </Popup>
-                    </Marker>
-                ))}
-            </MarkerClusterGroup>
-        </MapContainer>
-    );
-};
-export default TreesMap;
+// export default TreesMap;
+
+// function ProfilMap() {
+//     return (
+//         <div>
+//             <ProfilModal />
+//         </div>
+//     );
+// }
+
+// ReactDOM.render(<ProfilMap />, document.querySelector("#profil"));
